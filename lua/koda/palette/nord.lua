8911303a54ce428dcd36f8cd0018a29f0997c448
skrambol <skrambolito@gmail.com>
2026-07-09 09:07:35 +0800

    style(nord): added nord-like theme

---
+++ lua/koda/palette/nord.lua
@@ -0,0 +1,31 @@
+-- stylua: ignore
+---@class koda.Palette
+local palette = {
+  bg         = "#2e3440",
+  fg         = "#cdcecf",
+  dim        = "#5a657d",
+  line       = "#444c5e",
+  keyword    = "#abb1bb",
+  type       = "#abb1bb",
+  operator   = "#abb1bb",
+  comment    = "#60728a",
+  border     = "#ffffff",
+  emphasis   = "#ffffff",
+  func       = "#ffffff",
+  string     = "#a3be8c",
+  char       = "#a3be8c",
+  special    = "#ffffff",
+  const      = "#d89079",
+  highlight  = "#458ee6",
+  info       = "#81a1c1",
+  success    = "#a3be8c",
+  warning    = "#ebcb8b",
+  danger     = "#bf616a",
+  green      = "#a3be8c",
+  orange     = "#c9826b",
+  red        = "#bf616a",
+  pink       = "#bf88bc",
+  cyan       = "#88c0d0",
+}
+
+return palette
